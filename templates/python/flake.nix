86f15de0527d59d303ce6b454fcf9c3947394f7a
Alessandro Candido <candido.ale@gmail.com>
2025-06-22 10:30:11 +0200

    feat: Add plain python environment template

---
+++ templates/python/flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.devenv.flakeModule];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {
+        pkgs,
+        lib,
+        ...
+      }: {
+        devenv.shells.default = {
+          packages = with pkgs; [pre-commit poethepoet];
+
+          languages.python = {
+            enable = true;
+            venv = {
+              enable = true;
+              requirements = lib.splitString "\n" (builtins.readFile ./requirements.txt);
+            };
+          };
+        };
+      };
+    };
+}
